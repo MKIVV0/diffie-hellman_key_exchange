7e76e18371560d0bdfa18b32b19316b9f17c42bc
Matteo Huang <huangmatteo00@gmail.com>
2024-02-01 00:46:17 +0100

    added a time benchmark for p and g

--- src/main.c
+++ src/main.c
@@ -3,7 +3,11 @@
 #include "mbedtls/bignum.h"
 
 #include <stdio.h>
+#include <time.h>
 #include "number_gen.h"
+
+struct timespec start_time, end_time;
+long elapsed_time;
 
 int main(void) {
     mbedtls_entropy_context entropy;
@@ -16,8 +20,29 @@ int main(void) {
     init_mpi_vars(&p, &g);
     init_num_gen_contexts(&drbg, &entropy);
     
+    printf("Starting the generation of p...\n");
+    clock_gettime(CLOCK_MONOTONIC, &start_time);
+
     generate_random_number(&drbg, &entropy, "diffie-hellman", &p);
+
+    clock_gettime(CLOCK_MONOTONIC, &end_time);
+    elapsed_time = (end_time.tv_sec - start_time.tv_sec) * 1e9 +
+                   (end_time.tv_nsec - start_time.tv_nsec);
+    printf("The generation of p took %ld ns\n\n", elapsed_time);
+
+
+
+    printf("Starting the generation of g...\n");
+    clock_gettime(CLOCK_MONOTONIC, &start_time);
+
     generate_random_number(&drbg, &entropy, "diffie-hellman", &g);
+
+    clock_gettime(CLOCK_MONOTONIC, &end_time);
+    elapsed_time = (end_time.tv_sec - start_time.tv_sec) * 1e9 +
+                   (end_time.tv_nsec - start_time.tv_nsec);
+    printf("The generation of g took %ld ns\n", elapsed_time);
+
+
 
     mpi_to_str(&p, p_buff, &olen);
     mpi_to_str(&g, g_buff, &olen);
